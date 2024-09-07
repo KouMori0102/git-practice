b79ec17b3bff2cae6f229fe74e05c2f9fc36f8e1
35719040 <35719040@stn.nitech.ac.jp>
2024-09-07 20:31:23 +0900

    Initial Commit

---
+++ practice/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  practice
+//
+//  Created by 森下功樹 on 2024/09/07.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
